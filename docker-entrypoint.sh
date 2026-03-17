5812613f732b6f6a7f883aba56ba8d7e4fe6478d
Anwarkamal143 <anwarkamal1434@gmail.com>
2026-03-17 14:57:50 +0500

    entry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-    # ======================
-    # DATABASE MIGRATIONS
-    # ======================
-    echo "🚀 Starting database migrations..."
-    # bun run db-prod:migrate-with-seed
-    bun run db-prod:migrate-with-seed
-    echo "✅ Migrations completed successfully"
+# ======================
+# DATABASE MIGRATIONS
+# ======================
+echo "🚀 Starting database migrations..."
+# bun run db-prod:migrate-with-seed
+bun run db-prod:migrate-with-seed
+echo "✅ Migrations completed successfully"
 
 # ======================
 # MAIN APPLICATION
